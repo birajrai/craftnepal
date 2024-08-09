d41b48bb339e8eb5534a770aff1288f474f0fe2a
Biraj <70630924+birajrai@users.noreply.github.com>
2024-08-09 17:30:57 +0545

    Update Events.js

--- src/Events.js
+++ src/Events.js
@@ -1,28 +1,25 @@
 import React, { useState } from 'react';
-import endportal from './assets/images/endframe.png';
 import Countdown from 'react-countdown';
 
 const Events = () => {
-    const [open, setopen] = useState(true);
+    const [open, setOpen] = useState(true);
+
+    const date = new Date(1723875300000); // Convert timestamp to Date object
+
     return (
         <>
             {open ? (
                 <div className="events-box">
                     <div className="event-header">
                         <div className="event-title">END DIMENTION</div>
-                        <i class="fa fa-close event-close" onClick={() => setopen(false)}></i>
+                        <i className="fa fa-close event-close" onClick={() => setOpen(false)}></i>
                     </div>
                     <div className="event-body">
                         <div className="event-img-box">
-                            <img src={endportal} className="event-img"></img>
+                            <img src="./assets/images/endframe.png" alt="Event Image" className="event-img"></img>
                         </div>
-                        <div className="countdown-containner">
-                            <Countdown
-                                className="countdown"
-                                date={1723875300000}
-                                autoStart={true}
-                                daysInHours={false}
-                            ></Countdown>
+                        <div className="countdown-container">
+                            <Countdown className="countdown" date={date} key="countdown"></Countdown>
                             <div className="countdown-header">Opens In</div>
                         </div>
                     </div>
