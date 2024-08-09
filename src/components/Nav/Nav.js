b169a7959466f8ad9f669621d5c133bf6e7fce52
Biraj <70630924+birajrai@users.noreply.github.com>
2024-08-09 17:24:32 +0545

    Update Nav.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,206 +1,164 @@
-import React, { useEffect, useState } from "react";
-import "../../css/nav.css";
-import "../../css/mobile.css";
+import React, { useEffect, useState } from 'react';
+import './nav.module.css';
+import './mobile.module.css';
 
-import gallery from "../../assets/images/icons/gallery.png";
-import logo from "../../assets/svg/CN.svg";
-import home from "../../assets/images/home.png";
-import guide from "../../assets/images/guide.png";
-import stats from "../../assets/images/stats.png";
-import pin from "../../assets/images/pin.png";
+import Button from '../extra/UserComponent';
+import { useRouter } from 'next/router';
+import { clearScrollHistory } from '../extra/Clearscroll';
+import UserComponent from '../extra/UserComponent';
 
-import feed from "../../assets/images/feed.png";
-import Button from "../extra/UserComponent";
-import { Link } from "react-router-dom";
-import { clearScrollHistory } from "../extra/Clearscroll";
-import UserComponent from "../extra/UserComponent";
+const Nav = props => {
+    const { selected, focused } = props;
+    const [mobilenav, setmobilenav] = useState(false);
+    const router = useRouter();
 
-const Nav = (props) => {
-  const { selected, focused } = props;
-  const [mobilenav, setmobilenav] = useState(false);
-
-  var dynamic = false;
-  var STT = false;
-  const handleScroll = (e) => {
-    const scrollToTop = document.getElementById("scrollToTop");
-    if (window.scrollY > 500) {
-      if (scrollToTop && !STT) {
-        scrollToTop.classList.add("showScrollToTop");
-        STT = true;
-      }
-    } else {
-      if (scrollToTop && STT) {
-        scrollToTop.classList.remove("showScrollToTop");
-        STT = false;
-      }
-    }
-    if (mobilenav == true) return;
-    var element = document.getElementsByClassName("navbar")[0];
-    if (focused == false) {
-      if (window.scrollY > 10 && dynamic == false) {
-        element.classList.add("dynamicNav");
-        dynamic = true;
-      }
-      if (window.scrollY < 5 && dynamic == true && !focused) {
-        element.classList.remove("dynamicNav");
-        dynamic = false;
-      }
-    } else {
-      if (dynamic == false) {
-        element.classList.add("dynamicNav");
-        dynamic = true;
-      }
-    }
-  };
-  useEffect(() => {
-    document.addEventListener("scroll", (e) => handleScroll(e));
-  }, []);
-
-  const handleMenu = () => {
-    document
-      .getElementsByClassName("mobileNavLinks")[0]
-      .classList.toggle("openMobileNav");
-    document.getElementsByClassName("navbar")[0].classList.toggle("solidNav");
-    const button = document.getElementsByClassName("mybutton")[0];
-    if (button) button.classList.toggle("showButton");
-  };
-  return (
-    <>
-      <div className="navSpace"></div>
-      <div className={focused ? "navbar dynamicNav2" : "navbar"}>
-        <Link className="navLogo" to={"/"} onClick={clearScrollHistory()}>
-          <img src={logo} className="logo"></img>
-        </Link>
-        <div className="navLinks">
-          <Link
-            onClick={clearScrollHistory()}
-            to={"/stats"}
-            id="stats"
-            className={"nav " + (selected == "stats" ? "selected" : "")}
-          >
-            <img className="navImg" src={stats}></img>
-            <div>Stats</div>
-            <i class="fa fa-chevron-down navDrop"></i>
-
-            <Link
-              onClick={clearScrollHistory()}
-              to={"/leaderboard"}
-              className="subNav"
-            >
-              <div>Leaderboard</div>
-            </Link>
-          </Link>
-          <Link
-            to={"/feed"}
-            className={"nav " + (selected == "feed" ? "selected" : "")}
-            onClick={() => clearScrollHistory()}
-          >
-            <img className="navImg" src={feed}></img>
-            <div>Feed</div>
-          </Link>
-          <Link
-            to={"/guide"}
-            className={"nav " + (selected == "guide" ? "selected" : "")}
-            onClick={() => clearScrollHistory()}
-          >
-            <img className="navImg" src={guide}></img>
-            <div>Guide</div>
-          </Link>
-          <Link
-            to={"/gallery"}
-            className={"nav " + (selected == "gallery" ? "selected" : "")}
-            onClick={() => clearScrollHistory()}
-          >
-            <img className="navImg" src={gallery}></img>
-            <div>Fotos</div>
-          </Link>
-          <Link
-            to={"/map"}
-            className={"nav " + (selected == "map" ? "selected" : "")}
-            onClick={() => clearScrollHistory()}
-          >
-            <img className="navImg" src={pin}></img>
-            <div>Map</div>
-          </Link>
-        </div>
-        <UserComponent></UserComponent>
-        <i
-          class="material-icons menuIcon"
-          onClick={() => {
-            handleMenu();
-          }}
-        >
-          menu
-        </i>
-        <div className="mobileNavLinks">
-          <Link
-            to={"/"}
-            className={"nav " + (selected == "home" ? "selected" : "")}
-            onClick={() => clearScrollHistory()}
-          >
-            <img className="navImg" src={home}></img>
-            <div>Home</div>
-          </Link>
-          <div
-            id="statsmob"
-            className={
-              "subnavParent nav " + (selected == "stats" ? "selected" : "")
+    let dynamic = false;
+    let STT = false;
+    const handleScroll = e => {
+        const scrollToTop = document.getElementById('scrollToTop');
+        if (window.scrollY > 500) {
+            if (scrollToTop && !STT) {
+                scrollToTop.classList.add('showScrollToTop');
+                STT = true;
             }
-          >
-            <img className="navImg" src={stats}></img>
-            <div className="subNavContainnerMob">
-              <Link
-                className="mobsubNav"
-                onClick={() => clearScrollHistory()}
-                to={"/stats"}
-              >
-                Stats
-              </Link>
-              <Link
-                onClick={clearScrollHistory()}
-                to={"/leaderboard"}
-                className="mobsubNav"
-              >
-                Leaderboard
-              </Link>
+        } else {
+            if (scrollToTop && STT) {
+                scrollToTop.classList.remove('showScrollToTop');
+                STT = false;
+            }
+        }
+        if (mobilenav) return;
+        const element = document.getElementsByClassName('navbar')[0];
+        if (!focused) {
+            if (window.scrollY > 10 && !dynamic) {
+                element.classList.add('dynamicNav');
+                dynamic = true;
+            }
+            if (window.scrollY < 5 && dynamic && !focused) {
+                element.classList.remove('dynamicNav');
+                dynamic = false;
+            }
+        } else {
+            if (!dynamic) {
+                element.classList.add('dynamicNav');
+                dynamic = true;
+            }
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('scroll', handleScroll);
+        return () => {
+            document.removeEventListener('scroll', handleScroll);
+        };
+    }, [mobilenav, focused]);
+
+    const handleMenu = () => {
+        document.getElementsByClassName('mobileNavLinks')[0].classList.toggle('openMobileNav');
+        document.getElementsByClassName('navbar')[0].classList.toggle('solidNav');
+        const button = document.getElementsByClassName('mybutton')[0];
+        if (button) button.classList.toggle('showButton');
+    };
+
+    const navigateTo = path => {
+        router.push(path);
+        clearScrollHistory();
+    };
+
+    return (
+        <>
+            <div className="navSpace"></div>
+            <div className={focused ? 'navbar dynamicNav2' : 'navbar'}>
+                <div className="navLogo" onClick={() => navigateTo('/')}>
+                    <img src="/assets/svg/CN.svg" className="logo" alt="Logo" />
+                </div>
+                <div className="navLinks">
+                    <div
+                        className={'nav ' + (selected === 'stats' ? 'selected' : '')}
+                        onClick={() => navigateTo('/stats')}
+                    >
+                        <img className="navImg" src="/assets/images/stats.png" alt="Stats" />
+                        <div>Stats</div>
+                        <i className="fa fa-chevron-down navDrop"></i>
+                    </div>
+                    <div className="subNav" onClick={() => navigateTo('/leaderboard')}>
+                        <div>Leaderboard</div>
+                    </div>
+                    <div
+                        className={'nav ' + (selected === 'feed' ? 'selected' : '')}
+                        onClick={() => navigateTo('/feed')}
+                    >
+                        <img className="navImg" src="/assets/images/feed.png" alt="Feed" />
+                        <div>Feed</div>
+                    </div>
+                    <div
+                        className={'nav ' + (selected === 'guide' ? 'selected' : '')}
+                        onClick={() => navigateTo('/guide')}
+                    >
+                        <img className="navImg" src="/assets/images/guide.png" alt="Guide" />
+                        <div>Guide</div>
+                    </div>
+                    <div
+                        className={'nav ' + (selected === 'gallery' ? 'selected' : '')}
+                        onClick={() => navigateTo('/gallery')}
+                    >
+                        <img className="navImg" src="/assets/images/icons/gallery.png" alt="Gallery" />
+                        <div>Fotos</div>
+                    </div>
+                    <div className={'nav ' + (selected === 'map' ? 'selected' : '')} onClick={() => navigateTo('/map')}>
+                        <img className="navImg" src="/assets/images/pin.png" alt="Map" />
+                        <div>Map</div>
+                    </div>
+                </div>
+                <UserComponent />
+                <i className="material-icons menuIcon" onClick={handleMenu}>
+                    menu
+                </i>
+                <div className="mobileNavLinks">
+                    <div className={'nav ' + (selected === 'home' ? 'selected' : '')} onClick={() => navigateTo('/')}>
+                        <img className="navImg" src="/assets/images/home.png" alt="Home" />
+                        <div>Home</div>
+                    </div>
+                    <div id="statsmob" className={'subnavParent nav ' + (selected === 'stats' ? 'selected' : '')}>
+                        <img className="navImg" src="/assets/images/stats.png" alt="Stats" />
+                        <div className="subNavContainnerMob">
+                            <div className="mobsubNav" onClick={() => navigateTo('/stats')}>
+                                Stats
+                            </div>
+                            <div className="mobsubNav" onClick={() => navigateTo('/leaderboard')}>
+                                Leaderboard
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className={'nav ' + (selected === 'feed' ? 'selected' : '')}
+                        onClick={() => navigateTo('/feed')}
+                    >
+                        <img className="navImg" src="/assets/images/feed.png" alt="Feed" />
+                        <div>Feed</div>
+                    </div>
+                    <div
+                        className={'nav ' + (selected === 'guide' ? 'selected' : '')}
+                        onClick={() => navigateTo('/guide')}
+                    >
+                        <img className="navImg" src="/assets/images/guide.png" alt="Guide" />
+                        <div>Guide</div>
+                    </div>
+                    <div
+                        className={'nav ' + (selected === 'gallery' ? 'selected' : '')}
+                        onClick={() => navigateTo('/gallery')}
+                    >
+                        <img className="navImg" src="/assets/images/icons/gallery.png" alt="Gallery" />
+                        <div>Fotos</div>
+                    </div>
+                    <div className={'nav ' + (selected === 'map' ? 'selected' : '')} onClick={() => navigateTo('/map')}>
+                        <img className="navImg" src="/assets/images/pin.png" alt="Map" />
+                        <div>Map</div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <Link
-            to={"/feed"}
-            className={"nav " + (selected == "feed" ? "selected" : "")}
-            onClick={clearScrollHistory()}
-          >
-            <img className="navImg" src={feed}></img>
-            <div>Feed</div>
-          </Link>
-          <Link
-            to={"/guide"}
-            className={"nav " + (selected == "guide" ? "selected" : "")}
-            onClick={clearScrollHistory()}
-          >
-            <img className="navImg" src={guide}></img>
-            <div>Guide</div>
-          </Link>
-          <Link
-            to={"/gallery"}
-            className={"nav " + (selected == "guide" ? "selected" : "")}
-            onClick={clearScrollHistory()}
-          >
-            <img className="navImg" src={gallery}></img>
-            <div>Fotos</div>
-          </Link>
-          <Link
-            to={"/map"}
-            className={"nav " + (selected == "guide" ? "selected" : "")}
-            onClick={clearScrollHistory()}
-          >
-            <img className="navImg" src={pin}></img>
-            <div>Map</div>
-          </Link>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Nav;
