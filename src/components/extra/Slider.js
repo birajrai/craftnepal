53a6894445a1caf4cfca629b5b50c1495bb7f0fc
Biraj <70630924+birajrai@users.noreply.github.com>
2024-08-09 17:24:51 +0545

    Update Slider.js

--- src/components/extra/Slider.js
+++ src/components/extra/Slider.js
@@ -1,82 +1,73 @@
-import React, { useEffect, useState } from "react";
-import "./slider.css";
-import dharara from "../../assets/images/dharara.png";
-import home from "../../assets/images/home.png";
-import slider1 from "../../assets/images/slider1.png";
-import slider2 from "../../assets/images/slider2.png";
-import slider3 from "../../assets/images/slider3.png";
-import slider4 from "../../assets/images/slider4.png";
-import slider5 from "../../assets/images/slider5.png";
-
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import styles from './slider.module.css';
 const gallery = [
-  {
-    id: 1,
-    url: slider1,
-  },
-  {
-    id: 2,
-    url: slider2,
-  },
-  {
-    id: 3,
-    url: slider3,
-  },
-  {
-    id: 4,
-    url: slider4,
-  },
-  {
-    id: 5,
-    url: slider5,
-  },
+    {
+        id: 1,
+        url: '/assets/images/slider1.png',
+    },
+    {
+        id: 2,
+        url: '/assets/images/slider2.png',
+    },
+    {
+        id: 3,
+        url: '/assets/images/slider3.png',
+    },
+    {
+        id: 4,
+        url: '/assets/images/slider4.png',
+    },
+    {
+        id: 5,
+        url: '/assets/images/slider5.png',
+    },
 ];
 
 const Slider = () => {
-  var auto = true;
-  const [selected, setselected] = useState(1);
-  const changeImg = (id) => {
-    const parent = document.getElementById("slider");
-    const width = parent.clientWidth;
-    parent.scrollTo({ left: width * (id - 1), behavior: "smooth" });
-    const elements = document.getElementsByClassName("index");
-    for (let i = 0; i < elements.length; i++) {
-      const element = elements[i];
-      element.classList.remove("indexSelected");
-    }
-    document.getElementById(id + "i").classList.add("indexSelected");
-  };
-  useEffect(() => {
-    document.getElementById(1 + "i").classList.add("indexSelected");
-  }, []);
-  return (
-    <div className="sliderBox">
-      <div className="slider" id="slider">
-        {gallery.map((image) => {
-          return (
-            <div
-              key={image.id + "image"}
-              id={image.id + "image"}
-              className="slide"
-            >
-              <img src={image.url} className="slider-img"></img>
+    var auto = true;
+    const [selected, setselected] = useState(1);
+    const changeImg = id => {
+        const parent = document.getElementById('slider');
+        const width = parent.clientWidth;
+        parent.scrollTo({ left: width * (id - 1), behavior: 'smooth' });
+        const elements = document.getElementsByClassName('index');
+        for (let i = 0; i < elements.length; i++) {
+            const element = elements[i];
+            element.classList.remove('indexSelected');
+        }
+        document.getElementById(id + 'i').classList.add('indexSelected');
+    };
+
+    useEffect(() => {
+        document.getElementById(1 + 'i').classList.add('indexSelected');
+    }, []);
+
+    return (
+        <div className={styles.sliderBox}>
+            <div className={styles.slider} id="slider">
+                {gallery.map(image => {
+                    return (
+                        <div key={image.id + 'image'} id={image.id + 'image'} className={styles.slide}>
+                            <img src={image.url} className={styles.sliderImg} />
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-      <div className="slideIndex">
-        {gallery.map((image) => {
-          return (
-            <i
-              key={image.id + "i"}
-              id={image.id + "i"}
-              class={"fa fa-circle index "}
-              onClick={() => changeImg(image.id)}
-            ></i>
-          );
-        })}
-      </div>
-    </div>
-  );
+            <div className={styles.slideIndex}>
+                {gallery.map(image => {
+                    return (
+                        <i
+                            key={image.id + 'i'}
+                            id={image.id + 'i'}
+                            className={`fa fa-circle ${styles.index}`}
+                            onClick={() => changeImg(image.id)}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
 
 export default Slider;
