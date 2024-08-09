6a1ec7e4d84188ec07859186d7ec2fc4d40a2931
Biraj <70630924+birajrai@users.noreply.github.com>
2024-08-09 17:25:01 +0545

    Update DropDown.js

--- src/components/extra/dropdown/DropDown.js
+++ src/components/extra/dropdown/DropDown.js
@@ -1,44 +1,43 @@
-import React, { useState } from "react";
-import "./main.css";
-import CustomMarkDown from "../CustomMarkDown";
+import React, { useState } from 'react';
+import styles from './main.module.css'; // Updated for CSS Modules
+import CustomMarkDown from '../CustomMarkDown';
+
 const DropDown = ({ show, toggle, data }) => {
-  return (
-    <div className="dropdownBox">
-      <div className="dropdownHeader" onClick={() => toggle(data.title)}>
-        {data.title}
-        <i
-          className={`fa fa-chevron-${
-            show == data.title ? "up" : "down"
-          } downIcon`}
-        ></i>
-      </div>
-      <div
-        className={
-          show == data.title
-            ? "dropdownContent dropdownShow"
-            : "dropdownContent"
-        }
-      >
-        {show == data.title ? (
-          <div className="dropDownText">
-            <CustomMarkDown content={data.text}></CustomMarkDown>
-          </div>
-        ) : null}
-      </div>
-    </div>
-  );
+    return (
+        <div className={styles.dropdownBox}>
+            <div className={styles.dropdownHeader} onClick={() => toggle(data.title)}>
+                {data.title}
+                <i className={`fa fa-chevron-${show === data.title ? 'up' : 'down'} ${styles.downIcon}`}></i>
+            </div>
+            <div
+                className={
+                    show === data.title ? `${styles.dropdownContent} ${styles.dropdownShow}` : styles.dropdownContent
+                }
+            >
+                {show === data.title && (
+                    <div className={styles.dropDownText}>
+                        <CustomMarkDown content={data.text} />
+                    </div>
+                )}
+            </div>
+        </div>
+    );
 };
+
 const DropDowns = ({ data }) => {
-  const [selected, setSelected] = useState(null);
-  const toggle = (header) => {
-    if (header == selected) {
-      setSelected(null);
-    } else {
-      setSelected(header);
-    }
-  };
-  return data.map((dropdown) => (
-    <DropDown show={selected} toggle={toggle} data={dropdown}></DropDown>
-  ));
+    const [selected, setSelected] = useState(null);
+
+    const toggle = header => {
+        setSelected(prevSelected => (prevSelected === header ? null : header));
+    };
+
+    return (
+        <>
+            {data.map(dropdown => (
+                <DropDown key={dropdown.title} show={selected} toggle={toggle} data={dropdown} />
+            ))}
+        </>
+    );
 };
+
 export default DropDowns;
