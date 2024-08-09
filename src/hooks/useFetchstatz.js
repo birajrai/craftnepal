a962dbfbca4e846e0af30745074c08c7b6c3e5aa
Biraj <70630924+birajrai@users.noreply.github.com>
2024-08-09 17:24:10 +0545

    Update useFetchstatz.js

--- src/hooks/useFetchstatz.js
+++ src/hooks/useFetchstatz.js
@@ -1,29 +1,29 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import config from "../config.json";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import config from '../config.json';
 
-export const useFetchstatz = (username) => {
-  const [loading, setLoading] = useState(true);
-  const [statz, setStatz] = useState(null);
-  useEffect(() => {
-    if (username) {
-      setLoading(true);
-      getData();
-    }
-  }, [username]);
-  const getData = () => {
-    axios
-      .get(config.baseUrl + config.api + "statz/" + username)
-      .then((res) => {
-        setStatz(res.data);
-        setLoading(false);
-        console.log(res.data);
-      })
-      .catch((err) => {
-        setStatz({ err: "Something went wrong", data: null });
-        setLoading(false);
-      });
-  };
+export const useFetchstatz = username => {
+    const [loading, setLoading] = useState(true);
+    const [statz, setStatz] = useState(null);
+    useEffect(() => {
+        if (username) {
+            setLoading(true);
+            getData();
+        }
+    }, [username]);
+    const getData = () => {
+        axios
+            .get(config.baseUrl + config.api + 'statz/' + username)
+            .then(res => {
+                setStatz(res.data);
+                setLoading(false);
+                console.log(res.data);
+            })
+            .catch(err => {
+                setStatz({ err: 'Something went wrong', data: null });
+                setLoading(false);
+            });
+    };
 
-  return { loading, statz };
+    return { loading, statz };
 };
