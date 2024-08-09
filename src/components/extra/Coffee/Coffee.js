c1328a94b24205151e8ad9d37b89b8edf190d5ed
Biraj <70630924+birajrai@users.noreply.github.com>
2024-08-09 17:25:14 +0545

    Update Coffee.js

--- src/components/extra/Coffee/Coffee.js
+++ src/components/extra/Coffee/Coffee.js
@@ -1,26 +1,39 @@
-import React, { useEffect, useState } from "react";
-import "./main.css";
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import styles from './coffee.module.css';
+
 const Coffee = () => {
-  const [show, toggle] = useState(false);
-  useEffect(() => {
-    if (show) {
-    } else {
-    }
-  }, [show]);
-  return (
-    <div className={show ? "CoffeeContainner coffeeShow" : "CoffeeContainner"}>
-      <i class="fa fa-coffee coffee" onClick={() => toggle(!show)}></i>
-      <div className="coffeeHeader">
-        <b className="normaltext coffeeText ">Chya Kham na {":)"}</b>
-      </div>
-      <div className="coffContent">
-        <img
-          className="supportImage"
-          src="https://media.discordapp.net/attachments/725600128852820039/853620336716283934/IMG_20210613_184625.jpg?width=523&height=663"
-        ></img>
-      </div>
-    </div>
-  );
+    const [show, toggle] = useState(false);
+
+    useEffect(() => {
+        if (show) {
+            // do something when show is true
+        } else {
+            // do something when show is false
+        }
+    }, [show]);
+
+    return (
+        <div>
+            <Head>
+                <title>Coffee</title>
+            </Head>
+            <div className={show ? `${styles.CoffeeContainner} ${styles.coffeeShow}` : styles.CoffeeContainner}>
+                <i className="fa fa-coffee coffee" onClick={() => toggle(!show)}></i>
+                <div className={styles.coffeeHeader}>
+                    <b className={styles.normaltext}>Chya Kham na {':)'}</b>
+                </div>
+                <div className={styles.coffContent}>
+                    <img
+                        className={styles.supportImage}
+                        width={100} // specify the width of the image
+                        height={100} // specify the height of the image
+                        src="https://media.discordapp.net/attachments/725600128852820039/853620336716283934/IMG_20210613_184625.jpg?width=523&height=663"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Coffee;
