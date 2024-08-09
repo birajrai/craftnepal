e35ac7b3b7c06f74f29295d749acb5a682cd85a4
Biraj <70630924+birajrai@users.noreply.github.com>
2024-08-09 17:24:47 +0545

    Update UserComponent.js

--- src/components/extra/UserComponent.js
+++ src/components/extra/UserComponent.js
@@ -1,59 +1,59 @@
-import React, { useContext, useRef } from "react";
-import "./user.css";
-import discordIcon from "../../assets/images/icons/discord.png";
-import { UserContext } from "../../providers/UserProvider";
-import config from "../../config.json";
-import { Link } from "react-router-dom";
+import React, { useContext, useRef } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { UserContext } from '../../providers/UserProvider';
+import config from '../../config.json';
+import styles from './user.module.css';
+
 const UserComponent = () => {
-  const [user, gettingUser] = useContext(UserContext);
-  const userMenuref = useRef();
+    const [user, gettingUser] = useContext(UserContext);
+    const userMenuref = useRef();
 
-  return (
-    <div className="userComponent">
-      <dialog ref={userMenuref} className="userDialog">
-        <div className="dialogContainner">
-          <div className="dialogHeader">
-            <b className="">You are admin! This is what you can do {":)"}</b>
-            <i
-              class="fa fa-window-close dialog-close"
-              onClick={() => userMenuref.current.close("")}
-            ></i>
-          </div>
+    return (
+        <div className={styles.userComponent}>
+            <dialog ref={userMenuref} className={styles.userDialog}>
+                <div className={styles.dialogContainner}>
+                    <div className={styles.dialogHeader}>
+                        <b className="">You are admin! This is what you can do {':)'}</b>
+                        <i
+                            className="fa fa-window-close dialog-close"
+                            onClick={() => userMenuref.current.close('')}
+                        ></i>
+                    </div>
 
-          <Link to={"/manage-gallery"} className="dialogOptions">
-            Manage Gallery
-          </Link>
-          <Link to={"/guidecms"} className="dialogOptions">
-            GuideCMS
-          </Link>
-          <Link to={"/soon"} className="dialogOptions">
-            Soon
-          </Link>
-          <Link to={"/soon"} className="dialogOptions">
-            Soon
-          </Link>
-          <Link to={"/soon"} className="dialogOptions">
-            Soon
-          </Link>
+                    <Link href="/manage-gallery" className={styles.dialogOptions}>
+                        Manage Gallery
+                    </Link>
+                    <Link href="/guidecms" className={styles.dialogOptions}>
+                        GuideCMS
+                    </Link>
+                    <Link href="/soon" className={styles.dialogOptions}>
+                        Soon
+                    </Link>
+                    <Link href="/soon" className={styles.dialogOptions}>
+                        Soon
+                    </Link>
+                    <Link href="/soon" className={styles.dialogOptions}>
+                        Soon
+                    </Link>
+                </div>
+            </dialog>
+            {user == null || gettingUser || user.err ? (
+                <div className={styles.mybutton} onClick={() => (window.location = config.baseUrl + config.login)}>
+                    {/* <Image src={discordIcon} className={styles.loginIcon} /> */}
+                    Login
+                </div>
+            ) : (
+                <Image
+                    className={styles.userImage}
+                    onClick={() => (user.isAdmin ? userMenuref.current.showModal() : '')}
+                    width={100} // specify the width of the image
+                    height={100} // specify the height of the image
+                    src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`}
+                />
+            )}
         </div>
-      </dialog>
-      {user == null || gettingUser || user.err ? (
-        <div
-          className="mybutton"
-          onClick={() => (window.location = config.baseUrl + config.login)}
-        >
-          <img src={discordIcon} className="loginIcon"></img>
-          Login
-        </div>
-      ) : (
-        <img
-          className="userImage"
-          onClick={() => (user.isAdmin ? userMenuref.current.showModal() : "")}
-          src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}`}
-        ></img>
-      )}
-    </div>
-  );
+    );
 };
 
 export default UserComponent;
